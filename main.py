7b7838fd976559833d2c10de1b0d187424b8fb6e
JNemune <JNemune2002@gmail.com>
2022-11-27 12:53:30 +0330

    main has little changes

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ app = Client(
 
 
 @app.on_message(filters.chat(int(target1)))
-async def echo(client, m: pyrogram.types.messages_and_media.message.Message):
-    if checker(m.text):
+async def process(client, m: pyrogram.types.messages_and_media.message.Message):
+    if checker(m.text, "FAN1"):
         await m.reply("استفاده")
         await app.send_message(m.from_user.id, "سلام")
         await app.send_message(m.from_user.id, "من میتونم غذاتون رو استفاده کنم؟")
+        input("COMPLETE")
 
 
 if __name__ == "__main__":
