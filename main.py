e9c9905bd9a660948d4c7976fe8daa9ef328347f
JNemune <JNemune2002@gmail.com>
2022-12-06 13:51:49 +0330

    improving UI not test

--- main.py
+++ main.py
@@ -17,16 +17,29 @@ app = Client(
     api_hash,
     proxy={"scheme": "socks5", "hostname": "127.0.0.1", "port": 9051},
 )
-self_ = ["FAN1", "FAN2"][int(input("0: FAN1, 1: FAN2\n"))]
+self_ = None
 
 
 @app.on_message(filters.chat(int(target1)))
 async def process(client, m: pyrogram.types.messages_and_media.message.Message):
-    if checker(m.text, self_):
+    if self_ and checker(m.text, self_):
         await m.reply("استفاده")
         await app.send_message(m.from_user.id, "سلام")
         await app.send_message(m.from_user.id, "من میتونم غذاتون رو استفاده کنم؟")
-        input("COMPLETE")
+
+        self_ = None
+        app.send_message(admin_id, "انجام شد.")
+
+
+@app.on_message(filters.chat(int(admin_id)))
+async def runner(client, m: pyrogram.types.messages_and_media.message.Message):
+    if not self_:
+        m.reply("ربات در حال اجرا است. صبور باشید.")
+    if m.text in ["FAN1", "FAN2"]:
+        self_ = m.text
+        m.reply("درحال انجام ...")
+    else:
+        m.reply("متوجه نشدم :(")
 
 
 if __name__ == "__main__":
