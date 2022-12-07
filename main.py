c8d95527671f871e5267c3c2f971444201c225c9
JNemune <JNemune2002@gmail.com>
2022-12-07 07:57:14 +0330

    OOP

--- main.py
+++ main.py
@@ -5,42 +5,50 @@ from pyrogram import Client, filters
 
 from Checker import checker
 
-with open(path.join(".", "target", "config.txt"), "r") as f:
-    api_id, api_hash, admin_id, target1 = [
-        i.strip() for i in f.readlines() if i != "\n" and i[0] != "#"
-    ]
 
+class Run(object):
+    def __init__(self):
+        with open(path.join(".", "target", "config.txt"), "r") as f:
+            self.api_id, self.api_hash, self.admin_id, self.target1 = [
+                i.strip() for i in f.readlines() if i != "\n" and i[0] != "#"
+            ]
 
-app = Client(
-    "FreeFood",
-    api_id,
-    api_hash,
-    proxy={"scheme": "socks5", "hostname": "127.0.0.1", "port": 9051},
-)
-self_ = None
+        self.app = Client(
+            "FreeFood",
+            self.api_id,
+            self.api_hash,
+            proxy={"scheme": "socks5", "hostname": "127.0.0.1", "port": 9051},
+        )
+        self.self_ = None
 
+        self.process()
+        self.runner()
+        self.app.run()
 
-@app.on_message(filters.chat(int(target1)))
-async def process(client, m: pyrogram.types.messages_and_media.message.Message):
-    if self_ and checker(m.text, self_):
-        await m.reply("استفاده")
-        await app.send_message(m.from_user.id, "سلام")
-        await app.send_message(m.from_user.id, "من میتونم غذاتون رو استفاده کنم؟")
+    def process(self):
+        @self.app.on_message(filters.chat(int(self.target1)))
+        async def process(client, m: pyrogram.types.messages_and_media.message.Message):
+            if self.self_ and checker(m.text, self.self_):
+                await m.reply("استفاده")
+                await self.app.send_message(m.from_user.id, "سلام")
+                await self.app.send_message(
+                    m.from_user.id, "من میتونم غذاتون رو استفاده کنم؟"
+                )
 
-        self_ = None
-        app.send_message(admin_id, "انجام شد.")
+                self.self_ = None
+                await self.app.send_message(self.admin_id, "انجام شد.")
 
-
-@app.on_message(filters.chat(int(admin_id)))
-async def runner(client, m: pyrogram.types.messages_and_media.message.Message):
-    if not self_:
-        m.reply("ربات در حال اجرا است. صبور باشید.")
-    if m.text in ["FAN1", "FAN2"]:
-        self_ = m.text
-        m.reply("درحال انجام ...")
-    else:
-        m.reply("متوجه نشدم :(")
+    def runner(self):
+        @self.app.on_message(filters.chat(int(self.admin_id)))
+        async def runner(client, m: pyrogram.types.messages_and_media.message.Message):
+            if self.self_:
+                await m.reply("ربات در حال اجرا است. صبور باشید.")
+            elif m.text in ["FAN1", "FAN2"]:
+                self.self_ = m.text
+                await m.reply("درحال انجام ...")
+            else:
+                await m.reply("متوجه نشدم :(")
 
 
 if __name__ == "__main__":
-    app.run()
+    Run()
